e16f73764f6fed5c75a11fc3cb7328b2390fcef2
momomingzhi <momomingzhi@gmail.com>
2020-09-20 22:42:04 +0900

    백준 11724 연결 요소 개수 해결

--- 11724.cpp
+++ 11724.cpp
@@ -1,17 +1,37 @@
 #include <cstdio> 
 #include <string.h>
 #include<iostream>
+#include <queue>
 using namespace std;
 int N,M;
-int map[1001][499501];
-int visited[1000][499500];
-int cnt;
-void bfs(int x){
+int map[1001][1001];
+int visited[1001];
+int cnt,len;
+void dfs(int x){
+    visited[x] = 1;
     for(int y=1;y<=N;y++){
-        if(map[x][y]==1 && !visited[x][y]){
-            visited[x][y] = 1;
-            bfs(y);
+        if(map[x][y]==1){
+            if(!visited[y]){
+                dfs(y);
+            }
+            
         }
+    }
+}
+void bfs(int x){
+    queue<int> q;
+    visited[x] = 1;
+    q.push(x);
+    while(!q.empty()){
+        int current = q.front();
+        q.pop();
+        for(int i=1;i<=N;i++){
+            if(map[current][i] && !visited[i]){
+                visited[i] = 1; 
+                q.push(i);
+            }
+        }
+
     }
 }
 int main(){
@@ -19,21 +39,22 @@ int main(){
     for(int i=0;i<M;i++){
         int x,y;
         cin>>x>>y;
+        len = max(y,len);
         map[x][y]=1;
+        map[y][x]=1;
     }
     for(int i=1;i<=N;i++){
-        for(int j=1;j<=N;j++){
-            cnt++;
-            visited[i][j] = 1;
-            bfs(i);
-        }
+            if(!visited[i]){
+                cnt++;
+                bfs(i);
+            }
     }
     cout<<cnt<<endl;
     
-    // for(int i=1;i<=N;i++){
-    //     for(int j=1;j<=N;j++){
-    //         cout<<map[i][j]<<' ';
-    //     }
-    //     cout<<endl;
-    // }
 }
+/*
+생각하지 못한 부분
+각 정점의 방문 여부 체크는 visited가 1차원배열인데 2차원으로 생각했다.
+무방향 그래프이기 때문에 양쪽 정점 모두 연결
+
+*/
