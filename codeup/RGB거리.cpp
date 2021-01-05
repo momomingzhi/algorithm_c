781f0ee6ecf302cec976a5c68a706c751c287d9b
momomingzhi <momomingzhi@gmail.com>
2021-01-05 23:26:22 +0900

    3501 RGB 거리

---
+++ codeup/RGB거리.cpp
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+int n;
+int Min;
+int arr[15][3];
+bool color[3];
+void dfs(int idx, int depth, int sum)
+{
+    printf("앞쪽: arr[%d][%d] = %d sum:%d min:%d\n", depth, idx, arr[depth][idx], sum + arr[depth][idx], Min);
+    if (depth != 0 && idx != 0)
+    {
+        if (sum + arr[depth][idx] > Min)
+        {
+            return;
+        }
+    }
+
+    if (depth == n - 1)
+    {
+
+        if (Min > sum + arr[depth][idx])
+            Min = sum + arr[depth][idx];
+        printf("들어와서: arr[%d][%d] = %d sum:%d min:%d\n", depth, idx, arr[depth][idx], sum + arr[depth][idx], Min);
+        return;
+    }
+    if (idx == 0)
+    {
+        dfs(idx + 1, depth + 1, sum + arr[depth][idx]);
+        dfs(idx + 2, depth + 1, sum + arr[depth][idx]);
+    }
+    else if (idx == 1)
+    {
+        dfs(idx - 1, depth + 1, sum + arr[depth][idx]);
+        dfs(idx + 1, depth + 1, sum + arr[depth][idx]);
+    }
+    else if (idx == 2)
+    {
+        dfs(idx - 1, depth + 1, sum + arr[depth][idx]);
+        dfs(idx - 2, depth + 1, sum + arr[depth][idx]);
+    }
+    // if(idx == n){
+
+    //     return;
+    // }
+    // for(int i=idx;i<n;i++){
+
+    // }
+}
+int main()
+{
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    Min = 100000000;
+    if (n == 1)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            if (Min > arr[0][i])
+            {
+                Min = arr[0][i];
+            }
+        }
+    }
+    else
+        dfs(0, 0, 0);
+    cout << Min << endl;
+}
