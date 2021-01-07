ef812f692ed7f1bed093731b936503d9cc9a1476
DESKTOP-UI5I6K2\jang <myeongji_jang@tmax.co.kr>
2021-01-07 15:06:05 +0900

    dp로 푼 rgb거리

--- codeup/RGB거리.cpp
+++ codeup/RGB거리.cpp
@@ -3,6 +3,33 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+//dp로 풀었을때
+int dp[3][1000] = {
+    0,
+};
+int rgb[3][1000] = {
+    0,
+};
+int main()
+{
+    int n; //집개수
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> rgb[0][i] >> rgb[1][i] >> rgb[2][i];
+    dp[0][0] = rgb[0][0];
+    dp[1][0] = rgb[1][0];
+    dp[2][0] = rgb[2][0];
+    for (int i = 1; i < n; i++)
+    {
+        dp[0][i] = min({dp[1][i - 1], dp[2][i - 1]}) + rgb[0][i];
+        dp[1][i] = min({dp[0][i - 1], dp[2][i - 1]}) + rgb[1][i];
+        dp[2][i] = min({dp[0][i - 1], dp[1][i - 1]}) + rgb[2][i];
+    }
+    cout << min({dp[0][n - 1], dp[1][n - 1], dp[2][n - 1]});
+    return 0;
+}
+/*
+dfs로 풀었을 때
 int n;
 int Min;
 int arr[15][3];
@@ -72,3 +99,4 @@ int main()
 
     cout << Min << endl;
 }
+*/
