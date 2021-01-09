ba2832d689cd7c6cfbedde18cca33668c08c1be6
momomingzhi <momomingzhi@gmail.com>
2021-01-09 21:52:14 +0900

    4880:서울에서 경산까지

--- codeup/서울에서경산까지.cpp
+++ codeup/서울에서경산까지.cpp
@@ -2,27 +2,47 @@
 #include <algorithm>
 #include <iostream>
 using namespace std;
-int n, k, time[105][2], money[105][2];
-int dfs(int idx, int sum)
+int n, k, times[105][2], money[105][2], maxMoney;
+int dfs(int idx, int _time, int _money)
 {
-    if (sum >= k)
+    if (_time > k)
     {
 
         return 0;
     }
-    if (idx == n)
+    if (idx == n + 1)
     {
+        if (_money > maxMoney)
+        {
+            maxMoney = _money;
+        }
         return 0;
     }
-    dfs(idx + 1, 0);
-    dfs(idx + 1, 1);
+    if (times[idx - 1][0] < times[idx - 1][1] && money[idx - 1][0] > money[idx - 1][1])
+    {
+        //도보로 가면 무조건 이득
+        dfs(idx + 1, times[idx - 1][0] + _time, money[idx - 1][0] + _money);
+    }
+    else if (times[idx - 1][0] > times[idx - 1][1] && money[idx - 1][0] < money[idx - 1][1])
+    {
+        //자전거로 가면 무조건 이득
+        dfs(idx + 1, times[idx - 1][1] + _time, money[idx - 1][1] + _money);
+    }
+    else
+    {
+        //비교 불가일때s
+        dfs(idx + 1, times[idx - 1][0] + _time, money[idx - 1][0] + _money);
+        dfs(idx + 1, times[idx - 1][1] + _time, money[idx - 1][1] + _money);
+    }
 }
 int main()
 {
     cin >> n >> k;
     for (int i = 0; i < n; i++)
     {
-        cin >> time[i][0] >> money[i][0];
-        cin >> time[i][1] >> money[i][1];
+        cin >> times[i][0] >> money[i][0];
+        cin >> times[i][1] >> money[i][1];
     }
+    dfs(1, 0, 0);
+    cout << maxMoney << endl;
 }
